499e751c7bc0f2aaf5e06ca9d67c3d4c880445aa
Jean-Yves Vinet <jyvinet@hotmail.ca>
2016-08-03 15:40:02 -0400

    version number

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export let Footer = () => 
   <div className="pure-g footer">
-      <div className="pure-u-1-2"><div>Version 0.1.0</div></div>
+      <div className="pure-u-1-2"><div>Version 0.4.2</div></div>
       <div className="pure-u-1-2"><div style={{float: 'right'}}>JYV</div></div>
   </div>
